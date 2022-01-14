a2f2ed0a77d7c0c0be9fb71b2c3105e73bdd31ee
JulianaGSantana <jugsantana@icloud.com>
2022-01-14 10:42:56 -0300

    Adicionando tab bar e organizando pastas

---
+++ Minimu/Utils/Views/ProcessView.swift
@@ -0,0 +1,23 @@
+//
+//  ProcessView.swift
+//  Minimu
+//
+//  Created by Juliana Santana on 14/01/22.
+//
+
+import SwiftUI
+
+struct ProcessView: View {
+    var body: some View {
+        NavigationView{
+            ZStack {
+                Color.blue
+            } .navigationTitle("Process")
+        }
+    }
+}
+struct ProcessView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProcessView()
+    }
+}
