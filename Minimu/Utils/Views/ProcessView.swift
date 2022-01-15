e48acaa77a2498776e5e944ef9a38d6d776d313a
JulianaGSantana <jugsantana@icloud.com>
2022-01-15 12:59:15 -0300

    Criei os componentes AchievedCard e Checkmarker

--- Minimu/Utils/Views/ProcessView.swift
+++ Minimu/Utils/Views/ProcessView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+
 struct ProcessView: View {
     var body: some View {
         NavigationView{
-            ZStack {
-                Color.blue
-            } .navigationTitle("Process")
-        }
+            ScrollView(.vertical) {
+                VStack(alignment:.leading){
+                    
+                    Text("Goal")
+                        .font(.title.bold())
+                      .padding(.leading)
+                    
+                    Text("Material Minimalism")
+                        .font(.title3.bold())
+                        .padding(.leading)
+                    
+                    Text("Wardrobe")
+                        .font(.headline)
+                        .foregroundColor(Color.secondary)
+                        .padding(.leading)
+
+                    Checkmarker()
+                        
+
+           Text("Achievements")
+                        .font(.title.bold())
+                   
+                    Text("Achieved")
+                        .font(.title3.bold())
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: [GridItem(.fixed(0))], spacing: 20) {
+                            ForEach(0...4, id: \.self) { item in
+                                AchievedCard(imageName: "imageTeste", text: "oiiii")
+                            }
+                        }
+                        
+                    } .frame(height: 220)
+                    .ignoresSafeArea()
+                       
+                    Text("Blocked")
+                        .font(.title3.bold())
+            }.navigationTitle("Process")
+            }  .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+                .background(Color.backgroundColorGrey)
+                
+    }
     }
 }
 struct ProcessView_Previews: PreviewProvider {
